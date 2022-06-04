1cd7c7574f4120387a897be10811bc83d8948caf
innabatalova <batalovainna97@gmail.com>
2022-06-04 16:32:33 +0300

    Start: relocation from old repo

---
+++ handlebars.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  partials: "src/partials",
+  pages: "src/pages",
+};
---
+++ src/index.js
@@ -0,0 +1,2 @@
+//window.location.href = 'http://localhost:3000/src/pages/auth/auth.html';
+window.location.href = 'http://localhost:1234/src/pages/auth/auth.html';
