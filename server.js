c1106a4665a5b75ecf29de94f168fc57b74dc758
innabatalova <batalovainna97@gmail.com>
2022-06-19 19:54:49 +0300

    Fix: check stylelint

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
+
 const app = express();
 
 app.use(express.static(`${__dirname}/dist`));
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).sendFile(`${__dirname}/dist/index.html`);
 });
 
