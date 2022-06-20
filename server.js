fa6e3ef097aa7e8ba3ea734c029ec0f7b2fdd863
innabatalova <batalovainna97@gmail.com>
2022-06-20 16:30:29 +0300

    Fix: get server.js

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 
 const app = express();
 
 app.use(express.static(`${__dirname}/dist`));
 
-app.get('/', (req, res) => {
+app.get("/*", (req, res) => {
   res.status(200).sendFile(`${__dirname}/dist/index.html`);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Мой сервер запускается на ${process.env.PORT || 3000} порту!`);
-});
+app.listen(process.env.PORT || 3000, () => {});
