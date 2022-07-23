c5448f33f3e7a5dafc167f5994779cbffc90cd79
innabatalova <batalovainna97@gmail.com>
2022-07-23 19:27:13 +0300

    Fix: error merge

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -78,11 +78,7 @@ const validationForm = (): Record<string, string> | void => {
         return data;
       }
     } else {
-<<<<<<< HEAD
       return data;
-=======
-      console.log("Данные формы", data);
->>>>>>> deploy
     }
   } else {
     return undefined;
